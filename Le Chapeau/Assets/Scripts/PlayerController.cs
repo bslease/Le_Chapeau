f2c87c26650d2a3fb7d4bf96f22ab7bd6df570e2
bslease <bslease@gmail.com>
2020-05-19 11:56:07 -0700

    add controllable player objects
    done through page 38 of pdf

---
+++ Le Chapeau/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
+
+public class PlayerController : MonoBehaviourPunCallbacks
+{
+    [HideInInspector]
+    public int id;
+
+    [Header("Info")]
+    public float moveSpeed;
+    public float jumpForce;
+    public GameObject hatObject;
+
+    [HideInInspector]
+    public float curHatTime;
+
+    [Header("Components")]
+    public Rigidbody rig;
+    public Player photonPlayer;
+
+    // called when the player object is instantiated
+    [PunRPC]
+    public void Initialize(Player player)
+    {
+        photonPlayer = player;
+        id = player.ActorNumber;
+
+        GameManager.instance.players[id - 1] = this;
+
+        // give the first player the hat
+
+        // if this isn't our local player, disable physics as that's
+        // controlled by the user and synced to all other clients
+        if (!photonView.IsMine)
+            rig.isKinematic = true;
+    }
+
+
+    private void Update()
+    {
+        Move();
+
+        if (Input.GetKeyDown(KeyCode.Space))
+            TryJump();
+    }
+
+    void Move()
+    {
+        float x = Input.GetAxis("Horizontal") * moveSpeed;
+        float z = Input.GetAxis("Vertical") * moveSpeed;
+
+        rig.velocity = new Vector3(x, rig.velocity.y, z);
+    }
+
+    void TryJump()
+    {
+        Ray ray = new Ray(transform.position, Vector3.down);
+
+        if (Physics.Raycast(ray, 0.7f))
+            rig.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+    }
+}
